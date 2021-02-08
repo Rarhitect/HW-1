4d6aafd3a79c1f94a4ba164e4c5814bbab88be80
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 00:41:34 +0300

    new variable

--- New Task/main.cpp
+++ New Task/main.cpp
@@ -7,5 +7,6 @@ int main()
     std::cout << a-66 << std::endl;
     std::cout << "I hate this git-staff" << std::endl;
     std::cout << "But I have to deal with it" << std::endl;
+    int new_var = 1;
     return 0;
 }
