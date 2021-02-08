79d553ae2f05e72ee86d6a925c53a42ef9e6b296
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 00:19:08 +0300

    Added new commit

--- New Task/main.cpp
+++ New Task/main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Something new" << std::endl;
     int a = 77;
     std::cout << a-66 << std::endl;
+    std::cout << "I hate this git-staff" << std::endl;
     return 0;
 }
