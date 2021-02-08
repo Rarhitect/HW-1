1379df66146bd33fb586a43e84a241ba244aaa67
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 00:50:24 +0300

    new coef

--- New Task/main.cpp
+++ New Task/main.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-    std::cout << "CONFLICT" << std::endl;
+    std::cout << "Something new" << std::endl;
     //fix the err
     int a = 77;
     std::cout << a-66 << std::endl;
     std::cout << "I hate this git-staff" << std::endl;
     std::cout << "But I have to deal with it" << std::endl;
-    int new_var = 1;
+    int new_var = 7;
     return 0;
 }
