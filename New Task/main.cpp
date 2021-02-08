55d53f336915169dfdbf20aca6395864f1dced0f
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-08 23:57:50 +0300

    Initial Commit

---
+++ New Task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  New Task
+//
+//  Created by Alyaev Roman on 08.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
