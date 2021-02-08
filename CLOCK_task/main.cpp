d20ed7325bebd3519cf37b127c8dc991154bbd83
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 01:01:24 +0300

    Initial Commit

---
+++ CLOCK_task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CLOCK_task
+//
+//  Created by Alyaev Roman on 09.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
