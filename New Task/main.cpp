202450f252763c76777174746bb883d50cfe8f6b
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 00:22:19 +0300

    commit new branch

--- New Task/main.cpp
+++ New Task/main.cpp
@@ -6,5 +6,6 @@ int main()
     int a = 77;
     std::cout << a-66 << std::endl;
     std::cout << "I hate this git-staff" << std::endl;
+    std::cout << "But I have to deal with it" << std::endl;
     return 0;
 }
