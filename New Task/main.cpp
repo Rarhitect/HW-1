7743410fff4f55834f204933794aeead26ab7232
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 00:53:18 +0300

    in process

--- New Task/main.cpp
+++ New Task/main.cpp
@@ -3,11 +3,9 @@
 int main()
 {
     std::cout << "Something new" << std::endl;
-    //fix the err
     int a = 77;
     std::cout << a-66 << std::endl;
     std::cout << "I hate this git-staff" << std::endl;
     std::cout << "But I have to deal with it" << std::endl;
-    int new_var = 7;
     return 0;
 }
