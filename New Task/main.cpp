ed77f9cef012e5a71bcdc249151bf299b1f47988
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-09 22:41:51 +0300

    111

--- New Task/main.cpp
+++ New Task/main.cpp
@@ -5,7 +5,6 @@ int main()
     std::cout << "Something new" << std::endl;
     int a = 77;
     std::cout << a-56 << std::endl;
-    std::cout << a-44 << std::endl;
     std::cout << "I hate this git-staff" << std::endl;
     std::cout << "But I have to deal with it" << std::endl;
     return 0;
